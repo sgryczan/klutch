f4128e1a058f7453dc09708df03fec27651f7f27
Sebastian Gryczan <sgryczan@netapp.com>
2020-04-24 01:25:41 -0600

    cleanup

--- web/handlers/home_test.go
+++ web/handlers/home_test.go
@@ -13,15 +13,11 @@ func TestHomeHandler(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
 	rr := httptest.NewRecorder()
 	handler := http.HandlerFunc(HomeHandler)
 
-	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
-	// directly and pass in our Request and ResponseRecorder.
 	handler.ServeHTTP(rr, req)
 
-	// Check the status code is what we expect.
 	if status := rr.Code; status != http.StatusFound {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusFound)
